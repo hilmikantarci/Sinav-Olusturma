6054a6e561babeb7f5585bf96e94eb81693eb81b
Hilmi Kantarci <hilmikantarci@gmail.com>
2020-10-17 21:00:40 +0300

    First

---
+++ sinavolusturma/Controllers/ExamController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sinavolusturma.Controllers
+{
+    public class ExamController : Controller
+    {
+        public IActionResult Index()
+        {
+            WebRequest istek1 = HttpWebRequest.Create("https://www.wired.com/");
+            var cevap1 = istek1.GetResponse();
+            CultureInfo tr = new CultureInfo("tr-TR");
+            StreamReader donenbilgi1 = new StreamReader(cevap1.GetResponseStream(), Encoding.GetEncoding(tr.TextInfo.ANSICodePage));
+            string gelen1 = donenbilgi1.ReadToEnd();
+
+            int titlebaslangic = gelen1.IndexOf("<title>") + 7;
+
+            int titlebitis = gelen1.Substring(titlebaslangic).IndexOf("</title>");
+
+            return View();
+        }
+    }
+}
---
+++ sinavolusturma/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using sinavolusturma.Entities;
+
+namespace sinavolusturma.Controllers
+{
+    [Route("user")]
+    public class UserController : Controller
+    {
+        private DatabaseContext db;
+
+        public UserController(DatabaseContext _db)
+        {
+            db = _db;
+        }
+
+        [Route("")]
+        [Route("~/")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            ViewBag.users = db.Users.ToList();
+            var model = new User();
+            
+
+            return View();
+        }
+    }
+}
